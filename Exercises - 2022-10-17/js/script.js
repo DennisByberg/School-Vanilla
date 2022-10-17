e99ca6a463e5db6db02d53ea8009e49b9d0d3d50
Dennis Byberg <dennis.byberg@hotmail.com>
2022-10-17 13:24:13 +0200

    commit

--- Exercises - 2022-10-17/js/script.js
+++ Exercises - 2022-10-17/js/script.js
@@ -1,1 +1,11 @@
-console.log(window.location.href);
+// Variabler...
+let headline = document.querySelector("body h1");
+let allPTags = document.querySelectorAll("p");
+
+headline.innerText = "Found You";
+headline.style.color = "black";
+headline.style.backgroundColor = "gold";
+
+allPTags.forEach((element) => {
+  element.innerText = "Found You 2";
+});
