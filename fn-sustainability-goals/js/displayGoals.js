5ea821ce9862a1e9edd4eb1d1bb01bf4a0610a5b
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-24 19:47:42 +0100

    more logic added

--- fn-sustainability-goals/js/displayGoals.js
+++ fn-sustainability-goals/js/displayGoals.js
@@ -1,24 +1,35 @@
-import { data } from "./api.js";
+import { data } from "./api.js"; // Data = API-List
 
 const ul = document.querySelector("main ul");
+let userChoice = null;
 
 async function displayAllGoals() {
   const allGoals = await data;
   let amountOfGoalsArray = [];
 
   for (let i = 0; i < allGoals.length; i++) {
-    if (amountOfGoalsArray.includes(allGoals[i].goal)) {
-      // Do nothing...
-    } else {
+    if (!amountOfGoalsArray.includes(allGoals[i].goal)) {
       amountOfGoalsArray.push(allGoals[i].goal);
     }
   }
+
   for (let i = 0; i < amountOfGoalsArray.length; i++) {
     const liEl = `
 		<li>${amountOfGoalsArray[i]}</li>
-	`;
+    `;
     ul.insertAdjacentHTML("beforeend", liEl);
   }
+  const liEls = document.querySelectorAll("main ul li");
+  liEls.forEach((x) => {
+    x.addEventListener("click", (x) => {
+      ul.textContent = "";
+      userChoice = parseInt(x.target.innerHTML);
+
+      allGoals.forEach((x) => {
+        console.log(typeof x.code);
+      });
+    });
+  });
 }
 
 export { displayAllGoals };
