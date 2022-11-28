62319d2d9b1daf3f23da5c758dcd6ab31adc5d83
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-28 16:46:57 +0100

    5.4

---
+++ syntax n stuff/5-Loops/5.4.js
@@ -0,0 +1,7 @@
+// Loop out adding array with console.log(). Put what index each fruit has in front. Ex. 0. apple, 1. orange, etc.
+
+let fruits = ["apple", "orange", "pear", "kiwi", "pineapple"];
+
+for (let i = 0; i < fruits.length; i++) {
+  console.log(`Index: ${i}: ${fruits[i]}`);
+}
