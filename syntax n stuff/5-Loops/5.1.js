29af06b8a8acb7676d6cd9d979cc9b0746a92d13
Dennis Byberg <dennis.byberg@hotmail.com>
2022-11-28 16:40:32 +0100

    5.1

---
+++ syntax n stuff/5-Loops/5.1.js
@@ -0,0 +1,5 @@
+// Create a for loop that iterates 1000 times. For each iteration you should console.log() which turn you are on.
+
+for (let i = 1; i <= 1000; i++) {
+  console.log(`You are on turn: ${i}`);
+}
