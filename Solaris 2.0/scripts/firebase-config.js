a15cbf0bf22b046d187b0a05a790dbed014d9cdb
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-06 19:50:46 +0100

    new imports

---
+++ Solaris 2.0/scripts/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+} from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8f-pH5436Tqt1gMP2FihSQZJRply2lhA",
+  authDomain: "solaris-2.firebaseapp.com",
+  projectId: "solaris-2",
+  storageBucket: "solaris-2.appspot.com",
+  messagingSenderId: "531428510769",
+  appId: "1:531428510769:web:02f9fc2d67b8e1309b1910",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app); // db
