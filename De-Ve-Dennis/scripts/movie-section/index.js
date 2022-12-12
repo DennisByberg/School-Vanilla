027285572ccb694f6df17ab26fac636d199d647d
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-12 23:00:35 +0100

    implemented function addDeleteOnClick

--- De-Ve-Dennis/scripts/movie-section/index.js
+++ De-Ve-Dennis/scripts/movie-section/index.js
@@ -1,17 +1,15 @@
-import { getAllMovies } from "../firebase.js";
+import { getAllMovies, removeFromDatabase } from "../firebase.js";
 
 const moviesUl = document.querySelector("#movies-section ul");
 
 async function printAllMoviesToUl() {
   const movies = await getAllMovies();
-
   // clean Ul before printing it if updated...
   moviesUl.textContent = "";
-
   movies.forEach((movie) => {
     const liTemplate = `
     <div class="movie-container">
-      <button class="delete-btn">X</button>
+      <button data-movie-id="${movie.id}" class="delete-btn">X</button>
       <li>
         <p>Name: ${movie.data().name}</p>
         <p>Genre: ${movie.data().genre}</p>
@@ -19,9 +17,20 @@ async function printAllMoviesToUl() {
       </li>
     </div>
     `;
-
     moviesUl.insertAdjacentHTML("beforeend", liTemplate);
   });
+  addDeleteOnClick();
 }
 
+function addDeleteOnClick() {
+  const allButtons = document.querySelectorAll("#movies-section ul button");
+
+  allButtons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const movieID = e.target.getAttribute("data-movie-id");
+      removeFromDatabase(movieID);
+      printAllMoviesToUl();
+    });
+  });
+}
 export { printAllMoviesToUl };
