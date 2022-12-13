7ebf8ce463e973773b9485ebcdae17cb01848c6e
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-13 14:19:26 +0100

    finally it shows no movies in ul text when movies is below 1

--- De-Ve-Dennis/scripts/movie-section/index.js
+++ De-Ve-Dennis/scripts/movie-section/index.js
@@ -1,23 +1,25 @@
 import { getAllMovies, removeFromDatabase } from "../firebase.js";
 
 const moviesUl = document.querySelector("#movies-section ul");
+const noMoviesPTag = document.querySelector("#no-movies-to-see-txt");
 
 async function printAllMoviesToUl() {
   const movies = await getAllMovies();
-  moviesUl.textContent = "";
+  resetUl();
+
   if (movies.size > 0) {
     // clean Ul before printing it if updated...
     movies.forEach((movie) => {
       const liTemplate = `
-    <div class="movie-container">
+      <div class="movie-container">
       <button data-movie-id="${movie.id}" class="delete-btn">X</button>
       <li>
-        <p>Name: ${movie.data().name}</p>
-        <p>Genre: ${movie.data().genre}</p>
-        <p>Release Date: ${movie.data().releaseDate}</p>
+      <p>Name: ${movie.data().name}</p>
+      <p>Genre: ${movie.data().genre}</p>
+      <p>Release Date: ${movie.data().releaseDate}</p>
       </li>
-    </div>
-    `;
+      </div>
+      `;
       moviesUl.insertAdjacentHTML("beforeend", liTemplate);
     });
     addDeleteOnClick();
@@ -27,7 +29,14 @@ async function printAllMoviesToUl() {
 }
 
 function printNoMoviesInDB() {
-  moviesUl.innerHTML = `<p>Whoops... The list is empty</p>`;
+  moviesUl.style.display = "none";
+  noMoviesPTag.style.display = "flex";
+}
+
+function resetUl() {
+  moviesUl.style.display = "grid";
+  moviesUl.textContent = "";
+  noMoviesPTag.style.display = "none";
 }
 
 function addDeleteOnClick() {
