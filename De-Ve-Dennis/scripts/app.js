e4da01a6156e65e7f0d9b87b6a6d00e72c4904de
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-15 22:08:04 +0100

    imported welcomeSlider and calling it when  the program starts

--- De-Ve-Dennis/scripts/app.js
+++ De-Ve-Dennis/scripts/app.js
@@ -1,7 +1,9 @@
 import { addMovie, searchMovie, toggler } from "./head-section/index.js";
 import { printAllMoviesToUl } from "./movie-section/index.js";
+import { toggleWelcomeSlider } from "./sliders/welcomeSlider.js";
 
 // ON START...
+toggleWelcomeSlider();
 printAllMoviesToUl();
 
 // ON CLICK EVENTS...
