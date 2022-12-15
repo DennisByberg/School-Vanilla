e621c8a61dd42b6ec6afe355aae287e5233a3dd8
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-15 18:39:05 +0100

    changed function name

--- De-Ve-Dennis/scripts/app.js
+++ De-Ve-Dennis/scripts/app.js
@@ -1,10 +1,10 @@
-import { addToCollection, searchMovie, toggler } from "./head-section/index.js";
+import { addMovie, searchMovie, toggler } from "./head-section/index.js";
 import { printAllMoviesToUl } from "./movie-section/index.js";
 
 // ON START...
 printAllMoviesToUl();
 
 // ON CLICK EVENTS...
-addToCollection();
-toggler();
+addMovie();
 searchMovie();
+toggler();
