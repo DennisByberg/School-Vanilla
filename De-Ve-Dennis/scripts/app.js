c0d2f727b15be53d98151d368addfe69897e4ce8
Dennis Byberg <dennis.byberg@hotmail.com>
2022-12-16 13:49:24 +0100

    import + call of function devTips

--- De-Ve-Dennis/scripts/app.js
+++ De-Ve-Dennis/scripts/app.js
@@ -1,10 +1,12 @@
 import { addMovie, searchMovie, toggler } from "./head-section/index.js";
 import { printAllMoviesToUl } from "./movie-section/index.js";
 import { toggleWelcomeSlider } from "./sliders/welcomeSlider.js";
+import { devTips } from "./footer-section/index.js";
 
 // ON START...
 toggleWelcomeSlider();
 printAllMoviesToUl();
+devTips();
 
 // ON CLICK EVENTS...
 addMovie();
